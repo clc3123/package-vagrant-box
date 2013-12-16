288221cb3ce01ffae1536b5a39625002a4f27d03
clc3123 <clc3123@gmail.com>
2013-12-16 20:17:37 +0800

    fix

--- package.sh
+++ package.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 VBOX_VERSION="4.3.4"
-VBOX_SHARED_FOLDER=$(mount | grep vboxsf | awk 'print $3')
+VBOX_SHARED_FOLDER=$(mount | grep vboxsf | awk '{print $3}')
 CHEF_VERSION="11.8.2"
+
+done_echo()
+{
+  echo "================================================================================"
+  echo $1
+  echo "================================================================================"
+}
 
 echo > /home/vagrant/.vagrant_box_meta.yml <<METAYML
 packaging_date: $(date +%F)
@@ -12,7 +19,7 @@ virtualbox_guest_additions_version: ${VBOX_VERSION}
 METAYML
 chown vagrant:vagrant /home/vagrant/.vagrant_box_meta.yml
 chmod 0444 /home/vagrant/.vagrant_box_meta.yml
-echo "DONE saving /home/vagrant/.vagrant_box_meta.yml"
+done_echo "DONE saving /home/vagrant/.vagrant_box_meta.yml"
 
 echo > /etc/apt/sources.list <<SOURCE
 deb http://mirrors.163.com/ubuntu/ precise main restricted universe multiverse
@@ -24,20 +31,23 @@ deb-src http://mirrors.163.com/ubuntu/ precise-security main restricted universe
 SOURCE
 apt-get -y update
 apt-get -y dist-upgrade
-echo "DONE apt-get update & dist-upgrade"
+done_echo "DONE apt-get update & dist-upgrade"
 
 apt-get -y install build-essential vim-nox tree byobu htop nfs-common
-echo "DONE installing build-essential vim-nox tree byobu htop nfs-common"
+done_echo "DONE installing build-essential vim-nox tree byobu htop nfs-common"
 
 apt-get -y install dkms
-mount -o loop ${VBOX_SHARED_FOLDER}/VBoxGuestAdditions_${VBOX_VERSION}.iso /mnt
-/mnt/VBoxLinuxAdditions.run --nox11
-umount /mnt
-echo "DONE installing virtualbox guest additions ${VBOX_VERSION}"
+if ! (which VBoxControl && VBoxControl version | grep "\b${VBOX_VERSION}r")
+then
+  mount -o loop ${VBOX_SHARED_FOLDER}/VBoxGuestAdditions_${VBOX_VERSION}.iso /mnt
+  /mnt/VBoxLinuxAdditions.run --nox11
+  umount /mnt
+fi
+done_echo "DONE installing virtualbox guest additions ${VBOX_VERSION}"
 
 echo 'vagrant ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/vagrant
 chmod 0440 /etc/sudoers.d/vagrant
-echo "DONE setting vagrant as sudoer with nopasswd privilege"
+done_echo "DONE setting vagrant as sudoer with nopasswd privilege"
 
 if ! grep vagrant /home/vagrant/.ssh/authorized_keys > /dev/null 2>&1
 then
@@ -47,40 +57,46 @@ then
   chmod 0600 /home/vagrant/.ssh/authorized_keys
   chown -R vagrant:vagrant /home/vagrant/.ssh
 fi
-echo "DONE trusting vagrant ssh pubkey"
+done_echo "DONE trusting vagrant ssh pubkey"
 
-dpkg -i ${VBOX_SHARED_FOLDER}/chef_${CHEF_VERSION}-1.ubuntu.12.04_amd64.deb
-echo "Done installing chef-client ${CHEF_VERSION}"
+if ! (which chef-client && chef-client -v | grep "\b${CHEF_VERSION}\b")
+then
+  dpkg -i ${VBOX_SHARED_FOLDER}/chef_${CHEF_VERSION}-1.ubuntu.12.04_amd64.deb
+fi
+done_echo "DONE installing chef-client ${CHEF_VERSION}"
 
 umount ${VBOX_SHARED_FOLDER}
 rm -rf ${VBOX_SHARED_FOLDER}
-echo "DONE removing virtualbox shared folder"
+done_echo "DONE removing virtualbox shared folder"
 
 apt-get -y --purge autoremove
 apt-get --purge clean
-echo "DONE cleaning up apt things"
+done_echo "DONE cleaning up apt things"
 
-rm /var/lib/dhcp/*
-echo "DONE cleaning up dhcp leases"
+rm -f /var/lib/dhcp/*
+done_echo "DONE cleaning up dhcp leases"
 
-rm /etc/udev/rules.d/70-persistent-net.rules
-mkdir /etc/udev/rules.d/70-persistent-net.rules
-rm /lib/udev/rules.d/75-persistent-net-generator.rules
+if [ ! -d /etc/udev/rules.d/70-persistent-net.rules ]
+then
+  rm -f /etc/udev/rules.d/70-persistent-net.rules
+  mkdir /etc/udev/rules.d/70-persistent-net.rules
+fi
+rm -f /lib/udev/rules.d/75-persistent-net-generator.rules
 rm -rf /dev/.udev/
-echo "DONE cleaning up udev rules"
+done_echo "DONE cleaning up udev rules"
 
-rm /root/.bash_history
-rm /root/.viminfo
-rm /home/vagrant/.bash_history
-rm /home/vagrant/.viminfo
+rm -f /root/.bash_history
+rm -f /root/.viminfo
+rm -f /home/vagrant/.bash_history
+rm -f /home/vagrant/.viminfo
 rm -rf /home/vagrant/.byobu
 rm -rf /home/vagrant/.cache
-echo "DONE removing runtime user info"
+done_echo "DONE removing runtime user info"
 
 dd if=/dev/zero of=/empty bs=1M
 rm -f /empty
 dd if=/dev/zero of=/boot/empty bs=1M
 rm -f /boot/empty
-echo "DONE zeroing data on disk"
+done_echo "DONE zeroing data on disk"
 
 exit
